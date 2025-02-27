a8725de1495b583934e1cdf7d36057d28eea0f9f
Euan Turner <euanoturner@gmail.com>
2025-02-27 17:45:57 +0000

    test: Fix tests using TestClient as context manager

--- tests/test_infer_phonemes.py
+++ tests/test_infer_phonemes.py
@@ -5,20 +5,22 @@ from app.main import app
 from config.config import SUPPORTED_LANGUAGES
 
 
-client = TestClient(app)
-
 @pytest.mark.parametrize("lang", SUPPORTED_LANGUAGES)
 def test_valid_phoneme_inference(lang: str) -> None:
-    with open('tests/assets/hello.wav', 'rb') as f:
-        files = {"audio_file": f}
-        response = client.post(f"/api/v1/{lang}/infer_phonemes", files=files)
-    assert response.status_code == 200
-    assert "phonemes" in response.json()
+    with TestClient(app) as client:
+        with open('tests/assets/hello.wav', 'rb') as f:
+            files = {"audio_file": f}
+            data = {"attempt_word": "hello"}
+            response = client.post(f"/api/v1/{lang}/infer_phonemes", files=files, data=data)
+        assert response.status_code == 200
+        assert "phonemes" in response.json()
 
 def test_invalid_language() -> None:
-    response = client.post(
-        "/api/v1/xyz/infer_phonemes",
-        files={"audio_file": ("test.wav", b"testdata", "audio/wav")}
-    )
-    assert response.status_code == 400
-    assert response.json()["detail"].startswith("Unsupported language")
+    with TestClient(app) as client:
+        response = client.post(
+            "/api/v1/xyz/infer_phonemes",
+            files={"audio_file": ("test.wav", b"testdata", "audio/wav")},
+            data={"attempt_word": "hello"}
+        )
+        assert response.status_code == 400
+        assert response.json()["detail"].startswith("Unsupported language")
