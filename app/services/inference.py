23a8800d20a7bc17d9f8b86d1c0eca96f18229dd
Pedro Sa Fontes <pedrosafontes.psf@gmail.com>
2025-03-04 13:26:21 +0000

    refactor(Inference): :recycle: Remove unused prior files

--- app/services/inference.py
+++ app/services/inference.py
@@ -12,14 +12,14 @@ def get_inference_config(lang: str) -> Namespace:
     return Namespace(
         model=MODELS_FOR_LANGUAGES[lang], 
         lang_id=lang,
-        prior=f"config/languages/{lang}/prior.txt",
+        prior=None,
         device_id=-1,
         approximate=False
     )
 
 
-def infer_phones(wav_file: str, lang: str) -> List[str]:
-    """Performs phone inference using the recognizer model."""
+def infer_phonemes(wav_file: str, lang: str) -> List[str]:
+    """Performs phoneme inference using the recognizer model."""
     inference_config = get_inference_config(lang)
     
     recognizer = read_recognizer(inference_config_or_name=inference_config)
@@ -29,9 +29,3 @@ def load_phoneme_mapping(lang: str) -> Dict[str, str]:
     """Loads phoneme mapping for a given language from a JSON file."""
     with open(f'config/languages/{lang}/phone_to_phonemes.json', 'r') as f:
         return cast(Dict[str, str], json.load(f))
-
-def infer_phonemes(wav_file: str, lang: str) -> List[str]:
-    """Performs phoneme inference."""
-    phones = infer_phones(wav_file, lang)
-    phonemes = load_phoneme_mapping(lang)
-    return [phonemes.get(phone, "<unknown>") for phone in phones]
